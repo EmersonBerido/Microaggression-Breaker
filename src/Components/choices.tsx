99262c4326f92563f0046626c6fe6d956cb483ec
EmersonBerido <beridoemerson05@gmail.com>
2025-11-20 11:57:06 -0800

    Correct answer now shows

--- src/Components/choices.tsx
+++ src/Components/choices.tsx
@@ -1,36 +1,55 @@
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
 
 function Choices(props : any) {
   // Props contain: 
   //  level: theme, scenario, choices[], correct[], explanation
   //  index : number
 
+  const [selected, setSelected] = useState<number | null>(null);
+
   if (!props.level || !props.level.choices) {
     return null;
   }
 
-  const navigate = useNavigate();
+  const updateScore = () => {
+    props.setScore(prev => {
+      const newScore = prev + 1;
+      return newScore <= props.totalLevels ? newScore : prev;
+    })
+  }
+  
+  const reset = () => {
+    props.setIndex(prev => prev + 1);
+    setSelected(null);
+  }
 
-  const isCorrect = (totalLevels : number, level : any, idx : number) => {
-    if (level == null) {
+  const isCorrect = (level : any, idx : number) => {
+    if (level == null || selected != null) {
       return;
     }
 
-    if (level.correct.includes(idx)) {
-      props.setScore(prev => {
-        const newScore = prev + 1;
-        return newScore <= totalLevels ? newScore : prev;
-      })
-    }
+    setSelected(idx);
 
-    props.setIndex(props.index + 1);
+    if (level.correct.includes(idx)) {
+      // Correct Answer
+      updateScore();
+    }
   }
   
   // Load All Choices
   const options = props.level.choices.map((choice : string, idx : number) => {
+    const disabled = selected != null;
+    const isSelected = selected == idx;
+    const correctChoice = props.level.correct.includes(idx);
     return (
       <button
-        onClick={() => isCorrect(props.totalLevels, props.level, idx)}
+        className={`px-4 py-2 m-2 border rounded transition
+          ${disabled ? 'opacity-50 cursor-not-allowed' : 'bg-blue-500 text-white hover:bg-blue-600'}
+          ${isSelected && correctChoice ? 'bg-green-500 text-white' : ''}
+          ${isSelected && !correctChoice ? 'bg-red-500 text-white' : ''}
+          `}
+        onClick={() => isCorrect(props.level, idx)}
+        disabled={disabled}
         key={`level${props.index}choice${idx}`}
       >
         {choice}
@@ -38,9 +57,18 @@ function Choices(props : any) {
     )
   })
   return (
-    <>
+    <section>
       {options}
-    </>
+      {selected != null &&
+        <button 
+          onClick={() => reset()}
+          className="px-4 py-2 m-2 border rounded transition bg-white hover:bg-blue-500"
+        >
+          Next Question
+        </button>
+      }
+      
+    </section>
   )
 }
 export default Choices;
--- src/Routes/home_page.tsx
+++ src/Routes/home_page.tsx
@@ -1,14 +1,13 @@
 import {useNavigate} from "react-router-dom";
 
-
 function Home() {
   const navigate = useNavigate();
   return (
     <main>
-      <h1>Microaggression Breaker</h1>
-      <button onClick={() => navigate("/game")}>Play</button>
-      <button onClick={() => navigate("/community")}>Community</button>
-      <button onClick={() => navigate("/submit")}>Add Level</button>
+      <h1 className="text-white">Microaggression Breaker</h1>
+      <button className="px-4 py-2 bg-blue-500 text-white border border-white" onClick={() => navigate("/game")}>Play</button>
+      <button className="px-4 py-2 bg-blue-500 text-white border border-white" onClick={() => navigate("/community")}>Community</button>
+      <button className="px-4 py-2 bg-blue-500 text-white border border-white" onClick={() => navigate("/submit")}>Add Level</button>
     </main>
   )
 }
